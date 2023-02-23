3ab22b95dd8d7658c46fcdd0c2ab0ec0f80923ad
Moab Santos <moabsantos.msoftware@gmail.com>
2023-02-23 10:35:36 -0300

    feat: status api

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    const statusOk = 'api up'
+    console.log(statusOk)
     return 'Hello World!';
   }
 }
