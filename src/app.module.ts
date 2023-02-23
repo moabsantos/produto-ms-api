55beb8aeed5edfc162ef036a5be42b4cfcf26236
Moab Santos <moabsantos.msoftware@gmail.com>
2023-02-23 05:41:41 -0300

    feat: custos dia

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,46 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { AuthModule } from './_auth/auth.module';
+import { ClientAppModule } from './_client-app/module';
+import { EmailModule } from 'src/_mail/mailer.module';
+import { MyLoggerModule } from './_my-logger/module';
+import { RealmModule } from './_realm/module';
+import { DatabaseModule } from './_shared/database.module';
+import { UnidadeMedidaModule } from './crud-simples/unidade-medida/module';
+import { HttpModule } from '@nestjs/axios/dist/http.module';
+import { ProdutoModule } from './crud-simples/produto/module';
+import { FichaTecnicaModule } from './crud-simples/ficha-tecnica/module';
+import { EstagioModule } from './crud-simples/estagio/module';
+import { SetorModule } from './crud-simples/setor/module';
+import { EmpresaModule } from './crud-simples/empresa/module';
+import { CustosMensaisModule } from './crud-simples/custos-mensais/module';
+import { CustosDiaModule } from './crud-simples/custos-dia/module';
+
+
+
 @Module({
   imports: [
-    ConfigModule.forRoot()
+    HttpModule,
+    ConfigModule.forRoot(),
+
+    DatabaseModule,
+    EmailModule,
+    MyLoggerModule,
+
+    AuthModule,
+    RealmModule,
+    ClientAppModule,
+
+    EmpresaModule,
+    UnidadeMedidaModule,
+    EstagioModule,
+    SetorModule,
+    ProdutoModule,
+    FichaTecnicaModule,
+
+    CustosMensaisModule,
+    CustosDiaModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { ConfigGlobais } from './config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    //logger: new MyLoggerService(),
+    cors: {
+      origin: [
+        'http://localhost:3000',
+        'https://security.queavanca.com'
+      ],
+      credentials: true,
+    }
+  })
+
+
+  await app.listen(ConfigGlobais.ENV_PORT);
 }
 bootstrap();
