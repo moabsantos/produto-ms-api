2c2bf18acfdeec4b796f658cb9983968a96c0177
Moab Santos <moabsantos.msoftware@gmail.com>
2023-05-03 14:21:43 -0300

    feat: deposito requisicao

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,17 @@ import { ConfigGlobais } from './config';
 
 async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    //logger: new MyLoggerService(),
+    cors: {
+      origin: [
+        'http://localhost:3000',
+        'https://produto.queavanca.com'
+      ],
+      credentials: true,
+    }
+  })
 
-
-  app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: 'https://produto.queavanca.com',
-    credentials: true,
-  });
 
   await app.listen(ConfigGlobais.ENV_PORT);
 }
