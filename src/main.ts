551f2408621c3009fd25029bb904cfd43e47981b
Moab Santos <moabsantos.msoftware@gmail.com>
2023-05-03 13:00:56 -0300

    fix: calculo custos mensais

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import { AppModule } from './app.module';
 import { ConfigGlobais } from './config';
 
 async function bootstrap() {
-
+/*
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     //logger: new MyLoggerService(),
     cors: false
   })
+*/
 
+  const app = await NestFactory.create(AppModule);
 
   await app.listen(ConfigGlobais.ENV_PORT);
 }
