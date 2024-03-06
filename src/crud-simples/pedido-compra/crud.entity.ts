e19f93e788679fdcc114de7f2e2663ac69d368ad
Moab Santos <moabsantos.msoftware@gmail.com>
2024-03-06 15:51:58 -0300

    feat: flagProdutoAcaba

--- src/crud-simples/pedido-compra/crud.entity.ts
+++ src/crud-simples/pedido-compra/crud.entity.ts
@@ -2,7 +2,7 @@ import { BaseModelCrud } from "src/_shared/base-model-crud.entity";
 import { Column, Entity, Unique } from "typeorm";
 
 @Entity()
-@Unique("uniq_name", ["name", "realmId"])
+@Unique("uniq_name", ["name", "code", "realmId"])
 export class PedidoCompra extends BaseModelCrud {
 
 
