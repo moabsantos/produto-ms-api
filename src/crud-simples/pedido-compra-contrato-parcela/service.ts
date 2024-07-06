ce5509cb50c100a3d74888ba96998878955bafa0
Moab Santos <moabsantos.msoftware@gmail.com>
2024-07-06 11:38:21 -0300

    feat: geracao parcelas automaticamente

--- src/crud-simples/pedido-compra-contrato-parcela/service.ts
+++ src/crud-simples/pedido-compra-contrato-parcela/service.ts
@@ -106,6 +106,7 @@ export class PedidoCompraContratoParcelaService extends BaseCrudService{
 
         const contrato = await this.pedidoCompraContratoServ.getUnico(req, user, {id: param.pedidoCompraContratoId})
         if (!contrato) return {status: false, error: true, message: `Contrato não localizado [${param.pedidoCompraContratoId}]`}
+        console.log(contrato)
         if (!contrato['status'] || (contrato['status'] != 'Pendente') || contrato['gerarParcelaAutomaticamente'] != 1) return {status: false, error:false, message: "Contrato não está pendente"}
         if (!contrato['qtdParcelas'] || Number(contrato['qtdParcelas']) < 1) return {status: false, error:false, message: "Não há parcelas"}
 
