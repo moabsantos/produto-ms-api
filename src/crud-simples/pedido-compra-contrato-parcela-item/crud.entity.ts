c4fc84d528b2c205557b5a58c720db23b877d02e
Moab Santos <moabsantos.msoftware@gmail.com>
2024-07-13 16:16:35 -0300

    feat: item parcela

--- src/crud-simples/pedido-compra-contrato-parcela-item/crud.entity.ts
+++ src/crud-simples/pedido-compra-contrato-parcela-item/crud.entity.ts
@@ -11,7 +11,7 @@ export class PedidoCompraContratoParcelaItemParcelaItem extends BaseModelCrud {
     @Column({default: 0})
     sequencia: number;
     @Column()
-    Code: string;
+    code: string;
     @Column()
     name: string;
 
