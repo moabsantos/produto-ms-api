1c32eb4685aeda03d30d9c1f6fa8cf993d8903ca
Moab Santos <moabsantos.msoftware@gmail.com>
2025-02-12 07:10:43 -0300

    feat: requisicao sem local entrega

--- src/crud-simples/produto/service.ts
+++ src/crud-simples/produto/service.ts
@@ -126,7 +126,7 @@ export class ProdutoService extends BaseCrudService{
         }
 
         dto.name = this.produtoGrupo.name
-        if (this.produtoBase.name) dto.name = `${dto.name} ${this.produtoBase.name}`
+        if (this.produtoBase.name) dto.name = this.produtoBase.name
         if (this.produtoCor.name) dto.name = `${dto.name} ${this.produtoCor.name}`
         if (dto.endDescription) dto.name = `${dto.name} ${dto.endDescription}`
 
