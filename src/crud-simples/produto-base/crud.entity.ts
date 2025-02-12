90dc60ede617f033026a4d04548b0192d9768f9f
Moab Santos <moabsantos.msoftware@gmail.com>
2025-02-12 06:36:42 -0300

    feat: requisicao sem local entrega

--- src/crud-simples/produto-base/crud.entity.ts
+++ src/crud-simples/produto-base/crud.entity.ts
@@ -3,4 +3,21 @@ import { Column, Entity, Unique } from "typeorm";
 
 @Entity()
 @Unique("uniq_name", ["name", "realmId"])
-export class ProdutoBase extends BaseModelCrud {}
+export class ProdutoBase extends BaseModelCrud {
+
+    @Column()
+    endDescription: string;
+
+    @Column({nullable: true})
+    produtoGrupoId: number;
+
+    @Column({nullable: true})
+    produtoGrupoCode: string;
+
+    @Column({nullable: true})
+    produtoGrupoName: string;
+
+    @Column({nullable: true})
+    produtoGrupoSigla: string;
+
+}
--- src/crud-simples/produto-base/service.ts
+++ src/crud-simples/produto-base/service.ts
@@ -3,14 +3,57 @@ import { BaseCrudService } from "src/_shared/base-crud.service";
 
 import { ProdutoBase } from "./crud.entity";
 import { ProdutoBaseUser } from "./crud-user.entity";
+import { ProdutoGrupoService } from "../produto-grupo/service";
 
 export class ProdutoBaseService extends BaseCrudService{
 
+    private produtoGrupo: any;
+
     constructor (
         @InjectRepository(ProdutoBase) protected repo,
-        @InjectRepository(ProdutoBaseUser) protected repoUser)
-    {
+        @InjectRepository(ProdutoBaseUser) protected repoUser,
+    
+        private produtoGrupoServ: ProdutoGrupoService
+    ){
         super(repo, repoUser)
+    }
+
+    getDataFromDto(dto: any, user: any, model: ProdutoBase){
+
+        model.produtoGrupoName = this.produtoGrupo.name
+        model.produtoGrupoCode = this.produtoGrupo.code
+        model.produtoGrupoSigla = this.produtoGrupo.sigla
+        model.produtoGrupoId = this.produtoGrupo.id
+
+        model.endDescription = dto.endDescription
+
+        return super.getDataFromDto(dto, user, model)
+    }
+
+    async validate(dto: any, user: any): Promise<boolean>{
+        
+        const produtoGrp = await this.produtoGrupoServ.findByWhere({
+            id: dto.produtoGrupoId,
+            realmId: user.realmId
+        })
+
+        if (produtoGrp.length == 0){
+            this.logger.error(`O Grupo de Produto ${dto.produtoGrupoId} não foi encontrado`)
+            return false
+        }
+        this.produtoGrupo = produtoGrp[0]
+
+        
+
+        dto.name = this.produtoGrupo.name
+        if (dto.endDescription) dto.name = `${dto.name} ${dto.endDescription}`
+
+        if (!dto.name){
+            return false
+        }
+
+        return super.validate(dto, user)
+
     }
 
 }
